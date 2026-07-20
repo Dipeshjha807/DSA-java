4af3044457150a7ee8a843fe0ea16f2d21a75fe9
Dipeshjha807 <dipeshjha799@gmail.com>
2026-07-20 23:45:38 +0530

    today push

---
+++ binary search/patterns/inverted_pyramid.java
@@ -0,0 +1,18 @@
+public class inverted_pyramid {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
+
+            // for each row variable columns 
+   //fpr spaces
+            for (int j = 1; j <= i - 1; j++) {
+                System.out.print("  ");
+            }
+            ///for stars
+            for (int j = 1; j <= 2 * n - 2*i-1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ binary search/patterns/inverted_triangle.java
@@ -0,0 +1,15 @@
+public class inverted_triangle {
+    public static void main(String[] args) {
+        
+        int n=5;
+        for(int i=1;i<=n;i++){
+            /// for each row there is variable columns 
+             
+
+            for (int j=1;j<=n-i+1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
