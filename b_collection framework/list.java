3005e21208eb4c1dc9509f0a30bfd8d7022036f9
Dipeshjha807 <dipeshjha799@gmail.com>
2026-07-27 23:16:32 +0530

    list

---
+++ b_collection framework/list.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class list {
+
+    public static void main(String[] args) {
+        
+    
+    //lsit or collection is interface
+    // ArrayList is the concrete class for the  implementation
+
+    // sommon methods which we can perform in collections
+    ArrayList<Integer> list=new ArrayList<>();
+   //add()
+    list.add(1);
+    list.add(3);
+    list.add(6);
+    System.out.println(list);
+
+    //remove(index)
+list.remove(1);
+System.out.println(list);
+
+
+
+
+    // List<Integer> list=new ArrayList<>();
+    //collection<Integer> list=new ArrayList<>();
+
+}
+}
