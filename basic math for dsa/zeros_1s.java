f27f5c0f7ee3ff45c6bf6c78023d4b82ad094bc0
Dipeshjha807 <dipeshjha799@gmail.com>
2026-07-27 18:02:56 +0530

    zeros

---
+++ basic math for dsa/zeros_1s.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class zeros_1s {
+    public static void separateZerosAndOnes(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            while (arr[left] == 0 && left < right) {
+                left++;
+            }
+
+            while (arr[right] == 1 && left < right) {
+                right--;
+            }
+
+            if (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 1, 0, 1, 0, 0, 1};
+
+        separateZerosAndOnes(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
