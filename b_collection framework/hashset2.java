0b786044680bfbeed63f32f8cab940c4cf96b1a0
Dipeshjha807 <dipeshjha799@gmail.com>
2026-07-29 23:32:35 +0530

    hash2

---
+++ b_collection framework/hashset2.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class hashset2 {
+    public static void main(String[] args) {
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        set2.add(1);
+        set2.add(2);
+        set2.add(4);
+        System.out.println(set1);
+        set1.retainAll(set2);
+        System.out.println(set1);
+
+    }
+}
