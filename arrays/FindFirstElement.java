d31fe664740ca87e855ce0701fcbdbf8b6c1f8da
Dipeshjha807 <dipeshjha799@gmail.com>
2026-07-31 22:02:45 +0530

    reverse

--- arrays/FindFirstElement.java
+++ arrays/FindFirstElement.java
@@ -22,3 +22,4 @@ public class FindFirstElement {
        
 }
 }
+//O(n)
---
+++ arrays/reverse_array.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class reverse_array {
+
+    /// GFG question we have to do
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+
+            int temp = arr[i];
+
+            arr[i] = arr[j];
+
+            arr[j] = temp;
+            i++;
+
+            j--;
+
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
