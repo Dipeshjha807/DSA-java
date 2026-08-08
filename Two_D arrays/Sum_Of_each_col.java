4e18fcd09be85007becfb98ac344c038c7ca66b5
Dipeshjha807 <dipeshjha799@gmail.com>
2026-08-08 22:40:11 +0530

    wave

--- Two_D arrays/Sum_Of_each_col.java
+++ Two_D arrays/Sum_Of_each_col.java
@@ -2,7 +2,9 @@ public class Sum_Of_each_col {
     public static void main(String[] args) {
         int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
         int sum=0;
-        for(int j=0;j<arr[0].length;j++){
+        for(int j=0;j<arr[0].length;j++){              // Kyuki hume column-wise chalna hai.
+
+
             for(int i=0;i<arr.length;i++){
                 sum+=arr[i][j];
             }
