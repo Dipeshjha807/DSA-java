23ec7b93e4e8aed2de12cfd49f049e3ffc3aa5eb
Dipeshjha807 <dipeshjha799@gmail.com>
2026-08-09 10:18:41 +0530

    transpose

---
+++ Two_D arrays/transpose_of_a_a_Matrix.java
@@ -0,0 +1,26 @@
+public class transpose_of_a_a_Matrix {
+    public static void main(String[] args) {
+        
+        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }};
+
+       if(arr==null || arr.length==0){
+           System.out.println("Matrix is empty");
+           return;
+       }
+        int[][] transpose = new int[arr[0].length][arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                transpose[j][i] = arr[i][j];
+            }
+        }
+
+        System.out.println("Transpose of the matrix:");
+        for (int i = 0; i < transpose.length; i++) {
+            for (int j = 0; j < transpose[0].length; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
