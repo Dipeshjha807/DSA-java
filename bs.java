583bdb68e3fce250bea95bc343c65dc36b94b03a
Dipesh Jha <dipeshjha799@gmail.com>
2026-08-16 22:14:55 +0530

    Create bs.java

---
+++ bs.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class bs {
+
+    static int search(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        int target = 50;
+
+        int result = search(arr, target);
+
+        if (result != -1)
+            System.out.println("Element found at index: " + result);
+        else
+            System.out.println("Element not found");
+    }
+}
