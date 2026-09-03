16b0e03b68d7208a9cb7c6e6519e3cf98c600394
Dipeshjha807 <dipeshjha799@gmail.com>
2026-09-03 19:52:26 +0530

    painter partition problem

--- binary search/Book_Allocation.java
+++ binary search/Book_Allocation.java
@@ -13,13 +13,16 @@ public class Book_Allocation {
         int studentcount = 1;
         int pages = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (pages + arr[i] <= maxpages) {
+            if (pages + arr[i] <= maxpages) { // kya me current student ko curernt book assign kr skte he 
+                /// iska mtlb current book can be assigned to current student so pages me add kro
                 pages = pages + arr[i];
             } else {
-                studentcount++;
-                if (studentcount > k || arr[i] > maxpages) {
+                // current student ko current bok assign nhi kr skte so next student ko assign kro
+                studentcount++;  /// so naya studnet aa gya
+                if (studentcount > k || arr[i] > maxpages) { //here agar student k ki value se jada ho gya i.e 2 and jo mera currentbook page he i.e maxpages se bada abhi current wala book aya i. arr[i] wo bada ho gya mere maxpage se to wo limit bleach kr dega tlb jo abhi book aya wo itself mere maxpage se bada he s bleah kr dega 
                     return false;
                 } else {
+                    // can asssign new pages to new student
                     pages = 0;
                     pages = pages + arr[i];
                 }
@@ -30,6 +33,11 @@ public class Book_Allocation {
 
     // Isko static bana diya taaki main method se direct call ho sake
     public static int findpages(int[] arr, int k) {
+
+        if(arr.length <k) {
+            return -1; // Agar array empty hai to -1 return karo
+        }
+
         int n = arr.length;
         
         // Edge case: agar students books se zyada hain
@@ -52,6 +60,8 @@ public class Book_Allocation {
         int end = sum; // sum nikalne ke baad end assign kiya
         int ans = -1;
 
+
+        
         while (start <= end) {
             int mid = start + (end - start) / 2;
             if (isvalidanswer(arr, k, mid)) {
---
+++ binary search/painters_partition.java
@@ -0,0 +1,54 @@
+public class painters_partition {
+
+static boolean isvalid(int arr[], int k, int maxlength) {
+    /// mid is the max length of boards
+    /// k is the number of painters
+    /// arr is the array of boards
+    int paintercount = 1;
+    int boardsum = 0;
+    for (int i = 0; i < arr.length; i++) {
+        if(boardsum + arr[i] <= maxlength) {
+            boardsum += arr[i];
+        } else {
+            // current painter cannot paint this board, so we need a new painter
+            paintercount++;
+            if (paintercount > k || arr[i] > maxlength) {
+                return false;
+            }
+            boardsum = arr[i];
+        }
+    
+    }
+    return true;
+}
+
+    public static void main(String[] args) {
+        int[] arr = { 5,10,30,20,15 };
+        int k = 3;
+
+        int sum = 0;
+        int start = 0;
+       
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+             
+        }
+         int end = sum;
+          int ans = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (isvalid(arr, k, mid)) {
+                /// valid so store the potential answer in ans and move end to mid-1 becuse
+                /// after that mid all are possible ans but we want the min so mov end=mid-1
+                ans = mid;
+                end = mid - 1;
+            } else {
+                /// not valid so increase the mid value
+                start = mid + 1;
+            }
+        }
+        System.out.println("Minimum time to paint all boards: " + ans);
+
+    }
+}
