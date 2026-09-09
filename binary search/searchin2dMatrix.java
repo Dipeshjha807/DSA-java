0410f9ea474eb0e506946f86f6efada7d7b2b7fc
Dipeshjha807 <dipeshjha799@gmail.com>
2026-09-09 22:42:30 +0530

    modified

--- binary search/searchin2dMatrix.java
+++ binary search/searchin2dMatrix.java
@@ -1,5 +1,6 @@
 public class searchin2dMatrix {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) {  
         int[][] arr={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
         int totalrow=arr.length;
         int totalcol=arr[0].length;
@@ -10,8 +11,8 @@ public class searchin2dMatrix {
         int target=7;
         while(start<=end){
             int mid=start+(end-start)/2;
-            int row=mid/totalcol;  /// formulas for finiding row in 2d array
-            int col=mid%totalcol;   /// formulas for finding col in 2d array
+            int row=mid/totalcol;    /// formulas for finiding row in 2d array
+            int col=mid%totalcol;   ///  formulas for finding col in 2d array
 
             if(arr[row][col]==target){
                 System.out.println("Element found at index: "+row+","+col);
@@ -24,6 +25,6 @@ public class searchin2dMatrix {
                 end=mid-1;
             }
         }
-     
+        System.out.println("Element not found");
 }
 }
