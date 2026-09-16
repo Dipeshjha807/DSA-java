2cb88e569399d033abb20f64c2f4262342b2f594
Dipeshjha807 <dipeshjha799@gmail.com>
2026-09-16 23:58:47 +0530

    most frequesnt character in string

---
+++ String/most_frequest_chatacter.java
@@ -0,0 +1,29 @@
+public class most_frequest_chatacter {
+    public static void main(String[] args) {
+        String s = "my name is dipesh jha";
+        int[] freq = new int[26];
+
+        // Traverse over string and update their frequency accordingly
+        for (int i = 0; i < s.length(); i++) {
+            char currentchar = s.charAt(i);
+            
+            // 💡 Space ko ignore karne ke liye check lagana zaroori hai
+            if (currentchar >= 'a' && currentchar <= 'z') {
+                freq[currentchar - 'a']++;
+            }
+        }
+        
+        int maxfreq = -1;
+        char ans = ' '; // 💡 Yahan ek default value dena zaroori hai!
+
+        // Traverse over the freq array 
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > maxfreq) {
+                maxfreq = freq[i];
+                ans = (char)(i + 'a');
+            }
+        }
+        
+        System.out.println("Most frequent character: " + ans);
+    }
+}
