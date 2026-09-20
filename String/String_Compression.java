13b478e5568ea35cfcad35d2406bcd843d773504
Dipeshjha807 <dipeshjha799@gmail.com>
2026-09-20 19:47:14 +0530

    little change in stirng compression

--- String/String_Compression.java
+++ String/String_Compression.java
@@ -4,19 +4,24 @@ public class String_Compression {
         
         int readindex = 0;
         int writeindex = 0;
+///// loop char arrsay ko travel akrega 
 
         while (readindex < chars.length) {
             char currentchar = chars[readindex]; 
             int count = 0;
 
+
+            /// count duplicate character ko kr rha he 
             while (readindex < chars.length && chars[readindex] == currentchar) {
                 readindex++;
                 count++;
             }
+            // ab mere pass current character  and uska count dono he 
 
             chars[writeindex] = currentchar;
             writeindex++;
 
+            // now insert the count 
             if (count > 1) {
                 String countStr = String.valueOf(count);
                 for (char digit : countStr.toCharArray()) {
