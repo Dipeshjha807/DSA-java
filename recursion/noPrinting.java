a14785b94c8f6b2521743f0af5cd59465e9f8e2d
Dipeshjha807 <dipeshjha799@gmail.com>
2026-09-22 21:19:06 +0530

    recursion practice problems

---
+++ recursion/noPrinting.java
@@ -0,0 +1,16 @@
+public class noPrinting {
+
+    static void numbers(int n){
+        if (n==0){
+            return;
+        }
+        numbers(n-1);
+
+        System.out.print(n+ " ");
+        
+    }
+
+    public static void main(String[] args) {
+    numbers(10);
+    }
+}
