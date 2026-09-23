83ca261d96669d2f7ece1796155a5b8b900c9b60
Dipeshjha807 <dipeshjha799@gmail.com>
2026-09-24 00:09:41 +0530

    climbing stairs problem LC70

---
+++ recursion/climbing_stairs.java
@@ -0,0 +1,16 @@
+public class climbing_stairs {
+    
+    static int stairs(int  n){ /// yaha pe n= no of stairs to climb from 0 and the output jo he wo kitna possible way he stairs jane ka aisa 
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        int ans=stairs(n-1)+stairs(n-2);
+        return ans;
+    }
+public static void main(String[] args) {
+    System.out.println(stairs(5));
+}
+}
